f9254a96570a7fe72e11368cb93fad52a8f6dc73
luoxiao <luoxiao@yiside.com>
2018-08-01 23:45:32 +0800

    synchronized的基本使用

---
+++ src/com/thread/test0801/UseSynchronizedTest.java
@@ -0,0 +1,63 @@
+package com.thread.test0801;
+
+public class UseSynchronizedTest {
+    public static void main(String[] args) {
+        HasSelfPrivateNum numRef = new HasSelfPrivateNum();
+        ThreadA athread = new ThreadA(numRef);
+        athread.start();
+        ThreadB bthread = new ThreadB(numRef);
+        bthread.start();
+    }
+}
+
+
+class HasSelfPrivateNum {
+    private int num = 0;
+
+    //synchronized的作用
+    public synchronized void addI(String username) {
+        try {
+            if (username.equals("a")) {
+                num = 100;
+                System.out.println("a set over");
+                Thread.sleep(2000);
+            } else {
+                num = 200;
+                System.out.println("b set over");
+            }
+            System.out.println(username = " num " + num);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+class ThreadA extends Thread {
+    private HasSelfPrivateNum numRef;
+
+    public ThreadA(HasSelfPrivateNum numRef) {
+        super();
+        this.numRef = numRef;
+    }
+
+    @Override
+    public void run() {
+        super.run();
+        numRef.addI("a");
+    }
+}
+
+class ThreadB extends Thread {
+    private HasSelfPrivateNum numRef;
+
+    public ThreadB(HasSelfPrivateNum numRef) {
+        super();
+        this.numRef = numRef;
+    }
+
+    @Override
+    public void run() {
+        super.run();
+        numRef.addI("b");
+    }
+}
