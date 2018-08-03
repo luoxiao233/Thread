558434f2c904906b4cc1c621c3ae1823b7ed9726
luoxiao <home_dkx@126.com>
2018-08-03 18:03:48 +0800

    wait方法和notify方法的基本使用

---
+++ src/com/thread/test0803/waitAndNotify/MyList.java
@@ -0,0 +1,93 @@
+package com.thread.test0803.waitAndNotify;
+
+import java.util.ArrayList;
+import java.util.List;
+//java为每个Object都实现了wait()和notify()方法，不过方法必须用在synchronized同步的Object的临界区内
+
+public class MyList {
+
+    private static List list = new ArrayList();
+
+    public static void add() {
+        list.add("anyString");
+    }
+
+    public static int size() {
+        return list.size();
+    }
+
+
+    public static void main(String[] args) {
+        try {
+            Object lock = new Object();
+            ThreadA a = new ThreadA(lock);
+            a.start();
+            Thread.sleep(50);
+            ThreadB b = new ThreadB(lock);
+            b.start();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+class ThreadA extends Thread {
+    private Object lock;
+
+    public ThreadA(Object lock) {
+        this.lock = lock;
+    }
+
+    @Override
+    public void run() {
+        try {
+            synchronized (lock) {
+                if (MyList.size() != 5) {
+                    System.out.println("wait begin " + System.currentTimeMillis());
+                    lock.wait();
+                    System.out.println("wait end " + System.currentTimeMillis());
+                }
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+class ThreadB extends Thread {
+    private Object lock;
+
+    public ThreadB(Object lock) {
+        this.lock = lock;
+    }
+
+    @Override
+    public void run() {
+        try {
+            synchronized (lock) {
+                for (int i = 0; i < 10; i++) {
+                    MyList.add();
+                    if (MyList.size() == 5) {
+                        lock.notify();
+                        System.out.println("已发出通知!");
+                    }
+                    System.out.println("添加了" + (i + 1) + "个元素");
+                    Thread.sleep(1000);
+                }
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
